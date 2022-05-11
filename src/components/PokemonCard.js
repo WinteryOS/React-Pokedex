4cc668afacd87a89aae5dd3aa4f70d45a9f7ddd7
Will Sunderland <47872693+wSunderland@users.noreply.github.com>
2022-05-10 18:26:47 -0600

    API fix for pkmn sprites
    updated src to correctly display sprites

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -7,7 +7,7 @@ const PokemonCard = (props) => {
       <h3 className='card__title'>{props.name}</h3>
       <img
         className='card__img'
-        src={`https://pokeres.bastionbot.org/images/pokemon/${props.id}.png`}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`}
         alt='pokemon-pic'
       />
     </NavLink>
